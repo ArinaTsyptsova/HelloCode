6b2bd5cf7b0de9405af3e56054a6484c8e9b23d5
Arina <arinacypcova@gmail.com>
2023-07-02 20:09:58 +0700

    Example005_ifEse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марина")
+{
+    Console.WriteLine("Ура, это Госпожа!");
+
+}
+else
+{
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+}
